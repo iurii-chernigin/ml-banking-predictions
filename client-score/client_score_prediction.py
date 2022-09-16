d9ec933576847508530c19cfa2f0641f0ea93137
iurii-chernigin <chernigin.y@gmail.com>
2022-09-16 19:10:26 +0400

    Implemented a model for predicting the value of the field is_credit_closed

--- client-score/client_score_prediction.py
+++ client-score/client_score_prediction.py
@@ -13,47 +13,56 @@ def model_params(rf):
         .build()
 
 
-def prepare_data(df: DataFrame, assembler) -> DataFrame:
-    #TODO Здесь ваш код
+def prepare_data(df: DataFrame, features) -> DataFrame:
+    sex_idx = StringIndexer(inputCol='sex', outputCol='sex_idx')
+    df = sex_idx.fit(df).transform(df)
+    df = df.withColumn('married', df.married.cast(IntegerType()))
+    assembler = VectorAssembler(inputCols=features, outputCol='features')
     df = assembler.transform(df)
     return df
 
 
-def vector_assembler() -> VectorAssembler:
-    pass #TODO Здесь ваш код
-
-
 def build_random_forest() -> RandomForestClassifier:
-    pass #TODO Здесь ваш код
+    return RandomForestClassifier(labelCol='is_credit_closed', featuresCol='features')
 
 
 def build_evaluator() -> MulticlassClassificationEvaluator:
-    pass #TODO Здесь ваш код
+    return MulticlassClassificationEvaluator(
+        labelCol='is_credit_closed', predictionCol='prediction', metricName='accuracy'
+    )
 
 
 def build_tvs(rand_forest, evaluator, model_params) -> TrainValidationSplit:
-    pass #TODO Здесь ваш код
+    return TrainValidationSplit(
+        estimator=rand_forest,
+        estimatorParamMaps=model_params,
+        evaluator=evaluator,
+        trainRatio=0.8
+    )
 
 
-def train_model(train_df, test_df) -> (RandomForestClassificationModel, float):
-    assembler = vector_assembler()
-    train_pdf = prepare_data(train_df, assembler)
-    test_pdf = prepare_data(test_df, assembler)
+def train_model(train_df: DataFrame, test_df: DataFrame, features) -> (RandomForestClassificationModel, float):
+    
+    train_pdf = prepare_data(train_df, features)
+    test_pdf = prepare_data(test_df, features)
     rf = build_random_forest()
     evaluator = build_evaluator()
     tvs = build_tvs(rf, evaluator, model_params(rf))
+    
     models = tvs.fit(train_pdf)
-    best = models.bestModel
-    predictions = best.transform(test_pdf)
+    best_model = models.bestModel
+    predictions = best_model.transform(test_pdf)
     accuracy = evaluator.evaluate(predictions)
     print(f"Accuracy: {accuracy}")
-    print(f'Model maxDepth: {best._java_obj.getMaxDepth()}')
-    print(f'Model maxBins: {best._java_obj.getMaxBins()}')
-    return best, accuracy
+    print(f'Model maxDepth: {best_model._java_obj.getMaxDepth()}')
+    print(f'Model maxBins: {best_model._java_obj.getMaxBins()}')
+    return best_model, predictions, accuracy
 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName('PySparkMLJob').getOrCreate()
     train_df = spark.read.parquet("train.parquet")
     test_df = spark.read.parquet("test.parquet")
-    train_model(train_df, test_df)
+
+    features = ['age', 'sex_idx', 'married', 'salary', 'successfully_credit_completed', 'credit_completed_amount', 'active_credits', 'active_credits_amount']
+    train_model(train_df, test_df, features)
