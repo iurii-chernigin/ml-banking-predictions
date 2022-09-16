35139b1b5b095bf0d49535cda34c99fc4f50abc4
iurii-chernigin <chernigin.y@gmail.com>
2022-09-16 19:30:13 +0400

    Hot fixes

--- client-score/client_score_prediction.py
+++ client-score/client_score_prediction.py
@@ -13,13 +13,17 @@ def model_params(rf):
         .build()
 
 
-def prepare_data(df: DataFrame, features) -> DataFrame:
+def prepare_data(df: DataFrame, assembler) -> DataFrame:
     sex_idx = StringIndexer(inputCol='sex', outputCol='sex_idx')
     df = sex_idx.fit(df).transform(df)
     df = df.withColumn('married', df.married.cast(IntegerType()))
-    assembler = VectorAssembler(inputCols=features, outputCol='features')
     df = assembler.transform(df)
     return df
+
+
+def vector_assembler() -> VectorAssembler:
+    features = ['age', 'sex_idx', 'married', 'salary', 'successfully_credit_completed', 'credit_completed_amount', 'active_credits', 'active_credits_amount']
+    return VectorAssembler(inputCols=features, outputCol='features')
 
 
 def build_random_forest() -> RandomForestClassifier:
@@ -41,28 +45,27 @@ def build_tvs(rand_forest, evaluator, model_params) -> TrainValidationSplit:
     )
 
 
-def train_model(train_df: DataFrame, test_df: DataFrame, features) -> (RandomForestClassificationModel, float):
+def train_model(train_df: DataFrame, test_df: DataFrame) -> (RandomForestClassificationModel, float):
     
-    train_pdf = prepare_data(train_df, features)
-    test_pdf = prepare_data(test_df, features)
+    assembler = vector_assembler()
+    train_pdf = prepare_data(train_df, assembler)
+    test_pdf = prepare_data(test_df, assembler)
     rf = build_random_forest()
     evaluator = build_evaluator()
     tvs = build_tvs(rf, evaluator, model_params(rf))
     
     models = tvs.fit(train_pdf)
-    best_model = models.bestModel
-    predictions = best_model.transform(test_pdf)
+    best = models.bestModel
+    predictions = best.transform(test_pdf)
     accuracy = evaluator.evaluate(predictions)
     print(f"Accuracy: {accuracy}")
-    print(f'Model maxDepth: {best_model._java_obj.getMaxDepth()}')
-    print(f'Model maxBins: {best_model._java_obj.getMaxBins()}')
-    return best_model, predictions, accuracy
+    print(f'Model maxDepth: {best._java_obj.getMaxDepth()}')
+    print(f'Model maxBins: {best._java_obj.getMaxBins()}')
+    return best, accuracy
 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName('PySparkMLJob').getOrCreate()
     train_df = spark.read.parquet("train.parquet")
     test_df = spark.read.parquet("test.parquet")
-
-    features = ['age', 'sex_idx', 'married', 'salary', 'successfully_credit_completed', 'credit_completed_amount', 'active_credits', 'active_credits_amount']
-    train_model(train_df, test_df, features)
+    train_model(train_df, test_df)
